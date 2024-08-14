eb32e9da2a59ed28e471a476909ecbb1ff01dde4
snowdog.app <dog@snowdog.app>
2024-08-14 12:44:55 +0900

    Fix the TextEncoder's LR.

--- StackGAN.py
+++ StackGAN.py
@@ -416,17 +416,17 @@ class Solver:
         #print(summary(self.text_encoder.model.encoder))
         for param in self.text_encoder.parameters():
             param.requires_grad = False
-        for param in self.text_encoder.model.encoder.layer[-1].parameters():
-            param.requires_grad = True
+        #for param in self.text_encoder.model.encoder.layer[-1].parameters():
+        #    param.requires_grad = True
         
-        self.optimizer_G = optim.Adam([{'params': self.text_encoder.model.encoder.layer[-1].parameters(),
-                                        'lr': self.args.lr},
+        self.optimizer_G = optim.Adam([#{'params': self.text_encoder.model.encoder.layer[-1].parameters(),
+                                       # 'lr': self.args.lr},
                                        {'params': itertools.chain(self.stage1_g.parameters(),
                                                                   self.stage2_g.parameters()),
                                         'lr': 2 * self.args.lr}],
                                       betas=(0, 0.9))
-        self.optimizer_D = optim.Adam([{'params': self.text_encoder.model.encoder.layer[-1].parameters(),
-                                        'lr': self.args.lr * self.args.mul_lr_dis},
+        self.optimizer_D = optim.Adam([#{'params': self.text_encoder.model.encoder.layer[-1].parameters(),
+                                       # 'lr': self.args.lr * self.args.mul_lr_dis},
                                        {'params': itertools.chain(self.stage1_d.parameters(),
                                                                   self.stage2_d.parameters()),
                                         'lr': 2 * self.args.lr * self.args.mul_lr_dis}],
@@ -656,7 +656,7 @@ class Solver:
             epoch_loss = epoch_loss_G + epoch_loss_D
             
             print(f'Epoch[{self.epoch}]'
-                  + f' LR[G({self.scheduler_G.get_last_lr()[0]:.6f}, {self.scheduler_G.get_last_lr()[1]:.6f}) D({self.scheduler_D.get_last_lr()[0]:.6f}, {self.scheduler_D.get_last_lr()[1]:.6f})]'
+                  + f' LR[G({self.scheduler_G.get_last_lr()[0]:.6f}) D({self.scheduler_D.get_last_lr()[0]:.6f})]'
                   + f' G({epoch_loss_G}) + D({epoch_loss_D}) = {epoch_loss}')
             
             self.scheduler_G.step()
